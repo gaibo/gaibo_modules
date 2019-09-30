5fe96a72a8b2ed05d29deda269e2d6b33f508129
Zhang <gzhang@cboe.com>
2019-09-30 13:17:25 -0500

    just added an incredibly complex fix for duplicate series in cme_eod_file_reader.py

--- cme_eod_file_reader.py
+++ cme_eod_file_reader.py
@@ -112,6 +112,70 @@ def _handle_e_settlement_prices(data, tenor, trade_date_str):
     return data
 
 
+def _handle_duplicate_series(data):
+    """ Helper function for handling duplicate series
+    :param data: DataFrame from read_eod_file
+    :return: DataFrame with no duplicate series
+    """
+    data_indexed = data.set_index(['Last Trade Date', 'Put/Call', 'Strike Price']).sort_index()
+    # Find indexes where there are duplicates
+    # NOTE: .unique() is necessary since multiple dupes per series is possible
+    dupe_indexes = data_indexed.index[data_indexed.index.duplicated()].unique()
+    if len(dupe_indexes) == 0:
+        return data
+    else:
+        # Examine each series where duplicates exist
+        for dupe_index in dupe_indexes:
+            dupe_exp_pc = dupe_index[0:2]
+            dupe_pc = dupe_index[1]
+            dupe_strike = dupe_index[2]
+            # Get all potential series prices
+            dupe_prices = data_indexed.loc[dupe_index, 'Settlement']
+            # Get neighboring strikes' prices (avoid considering neighboring duplicates)
+            neighboring_prices = data_indexed.loc[dupe_exp_pc, 'Settlement']
+            neighboring_prices = neighboring_prices.loc[neighboring_prices.index.drop_duplicates(False)]
+            try:
+                dupe_prev_price = neighboring_prices[neighboring_prices.index < dupe_strike].iloc[-1]
+            except KeyError:
+                # No lower bound found
+                dupe_prev_price = 0
+            try:
+                dupe_next_price = neighboring_prices[neighboring_prices.index > dupe_strike].iloc[0]
+            except KeyError:
+                # No upper bound found
+                dupe_next_price = REASONABLE_DOLLAR_PRICE_LIMIT
+            if dupe_pc == 'C':
+                # For calls, lower strikes have higher prices
+                good_prices = dupe_prices[(dupe_prices >= dupe_next_price) &
+                                          (dupe_prices <= dupe_prev_price)].drop_duplicates()
+            else:
+                # For puts, higher strikes have higher prices
+                good_prices = dupe_prices[(dupe_prices >= dupe_prev_price) &
+                                          (dupe_prices <= dupe_next_price)].drop_duplicates()
+            n_good_prices = good_prices.count()
+            # If no reasonable prices found, remove all such series
+            if n_good_prices == 0:
+                print("WARNING: Duplicates found for series {} but NONE of the prices were reasonable."
+                      .format(dupe_index))
+                data_indexed = data_indexed.drop(dupe_index)    # Drop all
+                continue
+            # If 1 or more reasonable prices found, retain 1
+            if n_good_prices > 1:
+                print("WARNING: Duplicates found for series {} and MULTIPLE prices were reasonable ({})."
+                      .format(dupe_index, n_good_prices))
+            else:
+                print("WARNING: Duplicates fixed for series {}.".format(dupe_index))
+            if dupe_pc == 'C':
+                # Choose max, since next strike could also be a duplicate and lower priced
+                data_indexed.loc[dupe_index, 'Settlement'] = good_prices.max()
+            else:
+                # Choose min, since next strike could also be a duplicate and higher priced
+                data_indexed.loc[dupe_index, 'Settlement'] = good_prices.min()
+        # Earlier we overwrote corrected values to all duplicate indexes, so only retain first,
+        # then remove DataFrame indexing, then reset number index
+        return data_indexed.loc[~data_indexed.index.duplicated()].reset_index().reset_index(drop=True)
+
+
 def read_eod_file(tenor, trade_date_str, letter, file_dir=None, file_name=None):
     """ Read CME EOD Treasury files from disk and load them into consistently formatted DataFrames
     :param tenor: 2, 5, 10, or 30 (2-, 5-, 10-, 30-year Treasury options)
@@ -150,5 +214,7 @@ def read_eod_file(tenor, trade_date_str, letter, file_dir=None, file_name=None):
         data = _handle_e_settlement_prices(data, tenor, trade_date_str)
     else:
         data = _handle_fp_settlement_prices(data, tenor, trade_date_str)
+    # Handle duplicate series
+    data = _handle_duplicate_series(data)
 
     return data
