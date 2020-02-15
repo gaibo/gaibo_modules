9496b25b13dcf59d980dbd243c0b2ea471acf795
Zhang <gzhang@cboe.com>
2020-02-14 18:50:49 -0600

    bring treasury_futures_reader.py up to date with style of treasury_rates_reader. it now contains Bloomberg-querying functions

--- treasury_futures_reader.py
+++ treasury_futures_reader.py
@@ -1,10 +1,15 @@
 import pandas as pd
+import pdblp
+from cboe_exchange_holidays_v3 import datelike_to_timestamp
 
-TREASURY_FUT_CSV_FILEDIR = 'P:/PrdDevSharedDB/BBG Pull Scripts/'
+BLOOMBERG_PULLS_FILEDIR = 'P:/PrdDevSharedDB/BBG Pull Scripts/'
 TREASURY_FUT_CSV_FILENAME = 'treasury_futures_pull.csv'
 TENOR_TO_CODE_DICT = {2: 'TU', 5: 'FV', 10: 'TY', 30: 'US'}
-EXPMONTH_CODES_DICT = {1: 'F', 2: 'G', 3: 'H', 4: 'J', 5: 'K', 6: 'M',
-                       7: 'N', 8: 'Q', 9: 'U', 10: 'V', 11: 'X', 12: 'Z'}
+EXPMONTH_CODE_DICT = {1: 'F', 2: 'G', 3: 'H', 4: 'J', 5: 'K', 6: 'M',
+                      7: 'N', 8: 'Q', 9: 'U', 10: 'V', 11: 'X', 12: 'Z'}
+CODE_EXPMONTH_DICT = {'F': 1, 'G': 2, 'H': 3, 'J': 4, 'K': 5, 'M': 6,
+                      'N': 7, 'Q': 8, 'U': 9, 'V': 10, 'X': 11, 'Z': 12}
+QUARTER_CODE_LIST = ['H', 'M', 'U', 'Z']
 
 
 def month_to_quarter_shifter(month, shift=-1):
@@ -31,50 +36,115 @@ def undl_fut_quarter_month(opt_contr_month):
     return (((opt_contr_month-1) // 3) + 1) * 3     # month_to_quarter_shifter(opt_contr_month, shift=-1)
 
 
-def load_fut_prices(file_dir=None, file_name=None):
-    """ Read Treasury futures prices from disk and load them into a DataFrame
-    :param file_dir: optional directory to search for data file (overrides default directory)
-    :param file_name: optional exact file name to load from file_dir (overrides default file name)
+def fut_ticker(tenor, expiry_monthlike, expiry_type='options', use_single_digit_year=False):
+    """ Derive Bloomberg Treasury futures ticker from expiry of options or futures
+    :param tenor: 2, 5, 10, 30, etc. (-year Treasury futures)
+    :param expiry_monthlike: date-like representation of expiration month (precision only needed to month)
+    :param expiry_type: specify whether the expiry is 'options' or 'futures'
+    :param use_single_digit_year: set True to return single-digit year, used when querying for current year
+    :return: string Bloomberg ticker; e.g. 'TYM18 Comdty' for 10-year June futures in 2018
+    """
+    tenor_code = TENOR_TO_CODE_DICT[tenor]
+    expiry_month = datelike_to_timestamp(expiry_monthlike)
+    if expiry_type == 'options':
+        next_month_and_year = expiry_month + pd.DateOffset(months=1)
+        contract_year = next_month_and_year.year
+        contract_month = next_month_and_year.month
+    elif expiry_type == 'futures':
+        contract_year = expiry_month.year
+        contract_month = expiry_month.month
+    else:
+        raise ValueError("expiry_type must be either 'options' or 'futures'.")
+    quarter_code = EXPMONTH_CODE_DICT[undl_fut_quarter_month(contract_month)]
+    if use_single_digit_year:
+        year_code = f'{contract_year%10}'   # One digit only, useful for current year queries
+    else:
+        year_code = f'{contract_year%100:02d}'  # Two digits, useful for past years
+    ticker = tenor_code + quarter_code + year_code + ' Comdty'
+    return ticker
+
+
+def create_bloomberg_connection(debug=False, port=8194, timeout=25000):
+    """ Create pdblp/blpapi connection to the Bloomberg Terminal on this computer
+    :param debug: set True for verbose details on everything that comes through connection
+    :param port: network port; does not need to be changed
+    :param timeout: milliseconds for which function will attempt connection before timing out
+    :return:
+    """
+    # Start pdblp connection to Bloomberg
+    # NOTE: start with debugging using flag "debug=True", or set con.debug=True later on
+    con = pdblp.BCon(debug=debug, port=port, timeout=timeout)
+    con.start()
+    return con
+
+
+def pull_fut_prices(start_datelike, end_datelike, bloomberg_con=None,
+                    file_dir=BLOOMBERG_PULLS_FILEDIR, file_name=TREASURY_FUT_CSV_FILENAME):
+    """ Pull Treasury futures prices from Bloomberg Terminal and write them to disk
+    :param start_datelike: date-like representation of start date
+    :param end_datelike: date-like representation of end date
+    :param bloomberg_con: active pdblp Bloomberg connection; if None, runs create_bloomberg_connection()
+    :param file_dir: directory to write data file (overrides default directory)
+    :param file_name: exact file name to write to file_dir (overrides default file name)
+    :return: pd.DataFrame with all Treasury futures prices between start and end dates
+    """
+    start_date = datelike_to_timestamp(start_datelike)
+    end_date = datelike_to_timestamp(end_datelike)
+    # Create list of all Treasury futures Bloomberg tickers in use between start and end dates
+    ticker_list = []
+    for tenor_code in TENOR_TO_CODE_DICT.values():
+        for year in range(start_date.year, end_date.year):
+            # For all years up to but not including current year of end_date
+            for quarter_code in QUARTER_CODE_LIST:
+                ticker = tenor_code + quarter_code + f'{year:02d}' + ' Comdty'
+                ticker_list.append(ticker)
+        # For current year of end_date
+        for quarter_code in QUARTER_CODE_LIST:
+            ticker = tenor_code + quarter_code + f'{end_date.year%10}' + ' Comdty'
+            ticker_list.append(ticker)
+    # Get last price time-series of each ticker
+    bbg_start_dt = start_date.strftime('%Y%m%d')
+    bbg_end_dt = end_date.strftime('%Y%m%d')
+    if bloomberg_con is None:
+        bloomberg_con = create_bloomberg_connection()
+    fut_price_df = bloomberg_con.bdh(ticker_list, 'PX_LAST', start_date=bbg_start_dt, end_date=bbg_end_dt)
+    # Export
+    fut_price_df.to_csv(file_dir + file_name)
+    return fut_price_df
+
+
+def load_fut_prices(file_dir=BLOOMBERG_PULLS_FILEDIR, file_name=TREASURY_FUT_CSV_FILENAME):
+    """ Read Treasury futures prices from disk and load them into DataFrame
+    :param file_dir: directory to search for data file (overrides default directory)
+    :param file_name: exact file name to load from file_dir (overrides default file name)
     :return: pd.DataFrame with Treasury futures prices
     """
-    # Use default directory and file name
-    if file_dir is None:
-        file_dir = TREASURY_FUT_CSV_FILEDIR
-    if file_name is None:
-        file_name = TREASURY_FUT_CSV_FILENAME
-    # Load designated CSV containing Treasury futures prices
-    try:
-        return pd.read_csv(file_dir + file_name, index_col=0, parse_dates=True, header=[0, 1])
-    except FileNotFoundError:
-        print("ERROR: '{}' Treasury futures prices file not found.".format(file_dir+file_name))
-        return None
+    return pd.read_csv(file_dir + file_name, index_col=0, parse_dates=True, header=[0, 1])
 
 
-def get_fut_price(data, tenor, trade_date, opt_contr_year, opt_contr_month, opt_exp_date=None):
-    """ Retrieve Treasury options' underlying futures price from Bloomberg-exported data
-    :param data: Bloomberg-formatted dataset loaded via load_fut_prices
-    :param tenor: 2, 5, 10, 30, etc. (-year Treasury futures)
+def get_fut_price(trade_date, tenor, expiry_monthlike, expiry_type='options', data=None,
+                  contract_year=None, contract_month=None):
+    """ Retrieve Treasury futures price from Bloomberg-exported data
+        NOTE: contract_year and contract_month can be supplied together as an alternative to expiry_monthlike
     :param trade_date: trade date on which to get price
-    :param opt_contr_year: option contract year
-    :param opt_contr_month: option contract month
-    :param opt_exp_date: optional option expiration date (overrides opt_contr_year and opt_contr_month)
+    :param tenor: 2, 5, 10, 30, etc. (-year Treasury futures)
+    :param expiry_monthlike: date-like representation of expiration month (precision only needed to month);
+                             set None to use contract_year and contract_month
+    :param expiry_type: specify whether the expiry is 'options' or 'futures'
+    :param data: Bloomberg-formatted dataset loaded via load_fut_prices()
+    :param contract_year: options/futures contract year
+    :param contract_month: options/futures contract month
     :return: numerical price
     """
-    if opt_exp_date is not None:
-        # Override contract year and month
-        if isinstance(opt_exp_date, str):
-            opt_exp_date = pd.to_datetime(opt_exp_date)
-        next_month_and_year = opt_exp_date + pd.DateOffset(months=1)
-        opt_contr_year = next_month_and_year.year
-        opt_contr_month = next_month_and_year.month
-    tenor_code = TENOR_TO_CODE_DICT[tenor]
-    quarter_code = EXPMONTH_CODES_DICT[undl_fut_quarter_month(opt_contr_month)]
+    if expiry_monthlike is None:
+        expiry_monthlike = f'{contract_year}-{contract_month:02d}'
+    if data is None:
+        data = load_fut_prices()
     try:
-        year_code = '{:02d}'.format(opt_contr_year - 2000)  # Two digits for past years
-        ticker = tenor_code + quarter_code + year_code + ' Comdty'
+        ticker = fut_ticker(tenor, expiry_monthlike, expiry_type, use_single_digit_year=False)
         timeseries = data[ticker]['PX_LAST'].dropna()
     except KeyError:
-        year_code = '{}'.format((opt_contr_year - 2000) % 10)   # One digit only for most recent year
-        ticker = tenor_code + quarter_code + year_code + ' Comdty'
+        # Maybe case of ticker being in current year - use single digit year in ticker
+        ticker = fut_ticker(tenor, expiry_monthlike, expiry_type, use_single_digit_year=True)
         timeseries = data[ticker]['PX_LAST'].dropna()
     return timeseries.loc[trade_date]
