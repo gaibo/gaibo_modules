cb412f70ba771111d54f7d908f2641a19c57e268
Zhang <gzhang@cboe.com>
2020-02-28 12:01:30 -0600

    add kwargs to safe_requests_get. not a very good design - should find a good way to allow user to just not specify n_failures and use it as if it was requests.get()

--- web_tools.py
+++ web_tools.py
@@ -1,20 +1,25 @@
 import requests
 
 
-def safe_requests_get(api, n_failures=5):
+def safe_requests_get(url, n_failures=5, **kwargs):
     """ Loop requests.get() for an improved chance of success when using web APIs
-    :param api: web API on which to use requests.get()
+    :param url: web API URL on which to use requests.get()
     :param n_failures: max number of failures before giving up
+    :param kwargs: additional optional arguments for requests.get(); e.g. auth=('user', 'pass')
     :return: response from requests.get() or RuntimeError exception
     """
     failure_count = 0
     while failure_count < n_failures:
         try:
-            resp = requests.get(api)
+            resp = requests.get(url, **kwargs)
             break
         except ConnectionError:
             failure_count += 1
             print("safe_requests_get: failure #{failure_count} - ConnectionError.")
+        except requests.exceptions.ChunkedEncodingError:
+            failure_count += 1
+            print("safe_requests_get: failure #{failure_count} - ChunkedEncodingError;\n"
+                  "\taborted connection due to transmission time-out.")
     else:
-        raise RuntimeError(f"{api} query failed {failure_count} times in a row.")
+        raise RuntimeError(f"{url} query failed {failure_count} times in a row.")
     return resp
